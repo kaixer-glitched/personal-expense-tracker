d2caa9abb718f13550a8d74f9478d0cd617e55bf
kaixer-glitched <unmei917@gmail.com>
2026-06-04 23:36:51 +0800

    Just updated comments in ExpenseService layer.

--- src/main/java/ExpenseServices.java
+++ src/main/java/ExpenseServices.java
@@ -5,9 +5,6 @@ import java.util.*;
 @Service
 public class ExpenseServices {
 
-    // TODO: Clean up redundancy. Remove the 'expenses' ArrayList and rely only on 'expensesById' Map.
-    // TODO: To make this thread-safe for Spring Boot, change HashMap to ConcurrentHashMap.
-
     // we'll use an ArrayList to get all expenses
     List<Expense> expenses = new ArrayList<>();
 
@@ -17,32 +14,25 @@ public class ExpenseServices {
     // pre-gen ids
     Long firstId = 1L;
 
-    // we'll simply return a list that contains all expenses
     public List<Expense> getAllExpenses() { return expenses; }
 
     public Optional<Expense> getExpenseById(Long id) {
         return Optional.ofNullable(expensesById.get(id));
     }
 
-    // to be checked if working
     public double getTotalExpenses() {
+
         double total = 0;
 
         if (expenses.isEmpty()) return total;
 
-        for (Expense expense : expenses) {
-            total += expense.getAmount();
-        }
+        for (Expense expense : expenses) { total += expense.getAmount(); }
 
         return total;
     }
 
-    // TODO: REFINING ADD LOGIC:
-    // 1. Assign an ID automatically using the idCounter (e.g., expense.setId(idCounter++)).
-    // 2. Remove 'expenses.add(expense)' once the ArrayList is deleted.
-    // 3. Ensure it only saves to the map: expensesById.put(expense.getId(), expense).
-
     // we can eventually work on concurrent hashmap for thread-safety
+    // NOTE: concurrent or not, this one is just fine.
     public Expense addExpense(Expense expense) {
 
         expense.setId(firstId++);
